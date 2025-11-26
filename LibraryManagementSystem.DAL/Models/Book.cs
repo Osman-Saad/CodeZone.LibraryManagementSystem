9064be18a839425b7e435afb97808ed2b9c55c0e
Osman Saad <osman.saad7121@gmail.com>
2025-11-26 12:45:53 -0800

    Add core domain models + Author Class +Book Class + BorrowTransaction Class + Genre Enums

---
+++ LibraryManagementSystem.DAL/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using LibraryManagementSystem.DAL.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.DAL.Models
+{
+    public class Book
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public string? Description { get; set; }
+        public Guid AuthorId { get; set; }
+        public Author Author { get; set; }
+        public bool IsBorrowed { get; set; } = false;
+    }
+}
---
+++ LibraryManagementSystem.DAL/Models/BorrowTransaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.DAL.Models
+{
+    public class BorrowTransaction
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid BookId { get; set; }
+        public Book Book { get; set; } 
+        public DateTime BorrowedDate { get; set; }
+        public DateTime? ReturnedDate { get; set; }
+    }
+}
